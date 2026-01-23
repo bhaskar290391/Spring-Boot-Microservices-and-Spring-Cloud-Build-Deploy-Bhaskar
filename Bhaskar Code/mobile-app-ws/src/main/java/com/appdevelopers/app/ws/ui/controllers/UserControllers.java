0480634cabe1653c972293563ebbeaf0df24c31a
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-01-23 19:53:20 +0530

    accepting both xml and json format

--- Bhaskar Code/mobile-app-ws/src/main/java/com/appdevelopers/app/ws/ui/controllers/UserControllers.java
+++ Bhaskar Code/mobile-app-ws/src/main/java/com/appdevelopers/app/ws/ui/controllers/UserControllers.java
@@ -1,5 +1,6 @@
 package com.appdevelopers.app.ws.ui.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +23,9 @@ public class UserControllers {
 		return "Get user was called with userId page " + page + " limit : " + limit + " sort :" + sort;
 	}
 
-	@GetMapping("/{userId}")
+	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public UserRest getUsersById(@PathVariable String userId) {
-		UserRest returnValue=new UserRest(userId, "Bhaskar", "Mudaliyar", "maddy@gmail.com");
+		UserRest returnValue = new UserRest(userId, "Bhaskar", "Mudaliyar", "maddy@gmail.com");
 		return returnValue;
 	}
 
