9fbb77e577e8d82f990d79ae9c99606383536bbc
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-01-27 19:38:17 +0530

    adding gateway service

---
+++ Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.appdeveloperblogs.photoapp.users.ms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping("/health/status")
+	public String greet() {
+		return "Working";
+	}
+}
