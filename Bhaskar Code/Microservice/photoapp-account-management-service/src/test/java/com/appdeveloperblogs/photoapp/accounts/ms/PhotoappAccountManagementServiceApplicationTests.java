e7bfecefc1ea3e29e58437d6372026ed0abd314f
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-01-27 16:44:18 +0530

    user and account management service skeleton

---
+++ Bhaskar Code/Microservice/photoapp-account-management-service/src/test/java/com/appdeveloperblogs/photoapp/accounts/ms/PhotoappAccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdeveloperblogs.photoapp.accounts.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappAccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/PhotoappUserManagementServiceApplication.java
@@ -0,0 +1,15 @@
+package com.appdeveloperblogs.photoapp.users.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoappUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoappUserManagementServiceApplication.class, args);
+	}
+
+}
