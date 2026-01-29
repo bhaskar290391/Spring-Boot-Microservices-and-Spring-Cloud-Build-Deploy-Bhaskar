229ff32874129f2c8a1f10ab06a3b8d64a14f3b0
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-01-29 12:47:50 +0530

    saving of user data into h2 database

--- Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/controller/UserController.java
+++ Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.appdeveloperblogs.photoapp.users.ms.controller;
 
+import org.apache.http.HttpStatus;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.appdeveloperblogs.photoapp.users.ms.dto.UserDTO;
 import com.appdeveloperblogs.photoapp.users.ms.model.CreateUserRequestModel;
+import com.appdeveloperblogs.photoapp.users.ms.model.CreateUserResponseModel;
 import com.appdeveloperblogs.photoapp.users.ms.service.UserService;
 
 import jakarta.validation.Valid;
@@ -29,7 +35,16 @@ public class UserController {
 	}
 
 	@PostMapping
-	public String createUsers(@Valid @RequestBody CreateUserRequestModel userDetails) {
-		return "User Create was called ";
+	public ResponseEntity<CreateUserResponseModel> createUsers(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		
+		ModelMapper mapper =new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserDTO userDTO = mapper.map(userDetails, UserDTO.class);
+		
+		UserDTO createdValue = service.createUsers(userDTO);
+		
+		CreateUserResponseModel returnValue = mapper.map(createdValue, CreateUserResponseModel.class);
+		return new ResponseEntity<CreateUserResponseModel>(returnValue, null, HttpStatus.SC_CREATED);
 	}
 }
--- Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/service/UserServiceImpl.java
+++ Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/service/UserServiceImpl.java
@@ -2,15 +2,18 @@ package com.appdeveloperblogs.photoapp.users.ms.service;
 
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.appdeveloperblogs.photoapp.users.ms.Entity.UserEntity;
 import com.appdeveloperblogs.photoapp.users.ms.dao.UserRepository;
 import com.appdeveloperblogs.photoapp.users.ms.dto.UserDTO;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 
@@ -18,9 +21,18 @@ public class UserServiceImpl implements UserService {
 	public UserDTO createUsers(UserDTO userDetails) {
 
 		String userId = UUID.randomUUID().toString();
-
 		userDetails.setUserId(userId);
-		return null;
+
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserEntity userEntity = mapper.map(userDetails, UserEntity.class);
+		userEntity.setEncryptedPassword("test");
+
+		userEntity = repository.save(userEntity);
+
+		UserDTO returnValue = mapper.map(userEntity, UserDTO.class);
+
+		return returnValue;
 	}
 
 }
