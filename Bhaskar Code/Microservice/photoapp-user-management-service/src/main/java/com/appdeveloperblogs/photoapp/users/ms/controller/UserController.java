c2f14bb48d0f5ba3406f9a66a7dc761f6d258052
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-01-29 11:38:38 +0530

    creating create user endpoint and userrequest model

--- Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/controller/UserController.java
+++ Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/controller/UserController.java
@@ -3,8 +3,14 @@ package com.appdeveloperblogs.photoapp.users.ms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appdeveloperblogs.photoapp.users.ms.model.CreateUserRequestModel;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -12,9 +18,14 @@ public class UserController {
 
 	@Autowired
 	private Environment env;
-	
+
 	@GetMapping("/status/check")
 	public String greet() {
-		return "Working" +env.getProperty("local.server.port");
+		return "Working" + env.getProperty("local.server.port");
+	}
+
+	@PostMapping
+	public String createUsers(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		return "User Create was called ";
 	}
 }
---
+++ Bhaskar Code/Microservice/photoapp-user-management-service/src/main/java/com/appdeveloperblogs/photoapp/users/ms/model/CreateUserRequestModel.java
@@ -0,0 +1,57 @@
+package com.appdeveloperblogs.photoapp.users.ms.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CreateUserRequestModel {
+
+	@NotNull(message = "First Name cannot be null")
+	@Size(min = 2, message = "First Name can be atleast 2 characters")
+	private String firstName;
+
+	@NotNull(message = "Last Name cannot be null")
+	@Size(min = 2, message = "Last Name can be atleast 2 characters")
+	private String lastName;
+
+	@NotNull(message = "Password cannot be null")
+	@Size(min = 8, max = 16, message = "Password can be atleast 8 characters and less than 16 charecters")
+	private String password;
+
+	@NotNull(message = "Email cannot be null")
+	@Email()
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
